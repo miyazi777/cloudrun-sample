ff35959d0c8336a83e35980b1310bad40b6583e0
miyazi777 <m71203@gmail.com>
2023-09-23 22:12:16 +0900

    jsonレスポンス

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
+type Hello struct {
+	Title string
+	Desc  string
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(Hello{Title: "Hello", Desc: "World"})
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
